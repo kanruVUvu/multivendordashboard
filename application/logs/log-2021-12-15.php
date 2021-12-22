3fc97d3e1de9e27c8bf3cb0bf684dafb6e60cbee
Dheeraj Rao Pathur <dheeraj@bleamtech.com>
2021-12-23 01:34:06 +0530

    multivendordashboardcommit

---
+++ application/logs/log-2021-12-15.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-12-15 20:07:46 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:08:17 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:08:36 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:09:35 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:26 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:34 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:37 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:40 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:44 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:10:47 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:14:52 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:19:20 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:19:52 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:20:00 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:20:22 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:20:37 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:22:15 --> 404 Page Not Found: ../modules/admin/controllers//index
+ERROR - 2021-12-15 20:22:55 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 20:25:04 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-15 20:26:49 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 21:52:48 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 22:13:47 --> 404 Page Not Found: /index
+ERROR - 2021-12-15 22:13:48 --> Could not find the language line "vendor_home_page"
---
+++ application/logs/log-2021-12-16.php
@@ -0,0 +1,62 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-12-16 06:04:45 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:04:50 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-16 06:05:42 --> Severity: Warning --> Undefined array key "shop_categorie" C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\vendor\models\Products_model.php 49
+ERROR - 2021-12-16 06:05:42 --> Severity: Warning --> Undefined array key "brand_id" C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\vendor\models\Products_model.php 52
+ERROR - 2021-12-16 06:05:42 --> Query error: Column 'shop_categorie' cannot be null - Invalid query: INSERT INTO `products` (`image`, `shop_categorie`, `quantity`, `position`, `brand_id`, `folder`, `vendor_id`, `time`) VALUES ('fulllionimage.png', NULL, '10', 'test', NULL, '1639631098', '1', 1639631142)
+ERROR - 2021-12-16 06:09:35 --> Image Upload Error: <p>You did not select a file to upload.</p>
+ERROR - 2021-12-16 06:09:45 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-16 06:10:18 --> Severity: Warning --> Undefined array key "brand_id" C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\vendor\models\Products_model.php 52
+ERROR - 2021-12-16 06:10:39 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:11:25 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:11:36 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 06:11:36 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 06:22:21 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 06:22:21 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 06:22:29 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:22:29 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:22:30 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:22:30 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:22:30 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:22:30 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:25:15 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 06:28:57 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 09:14:42 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-16 09:14:59 --> Could not find the language line "vendor_home_page"
+ERROR - 2021-12-16 09:40:39 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 09:40:39 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 09:44:01 --> Severity: error --> Exception: array_count_values(): Argument #1 ($array) must be of type array, null given C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\views\templates\redlabel\view_product.php 64
+ERROR - 2021-12-16 09:44:45 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 09:44:45 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 09:49:14 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 09:49:14 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 09:50:15 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 09:50:15 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 09:50:15 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 09:50:15 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 10:01:35 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 10:01:35 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 10:07:41 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:41 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:41 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:42 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:42 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:42 --> 404 Page Not Found: /index
+ERROR - 2021-12-16 10:07:55 --> 404 Page Not Found: ../modules/admin/controllers/blog//index
+ERROR - 2021-12-16 10:17:04 --> Severity: error --> Exception: array_count_values(): Argument #1 ($array) must be of type array, null given C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\views\templates\redlabel\view_product.php 64
+ERROR - 2021-12-16 10:17:21 --> Severity: Warning --> Trying to access array offset on value of type null C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\views\templates\redlabel\checkout.php 46
+ERROR - 2021-12-16 10:17:34 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 10:17:34 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 10:31:53 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 10:31:53 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 10:32:19 --> Severity: 8192 --> Required parameter $limit follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 34
+ERROR - 2021-12-16 10:32:19 --> Severity: 8192 --> Required parameter $page follows optional parameter $lang C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\application\modules\admin\models\Blog_model.php 0
+ERROR - 2021-12-16 10:44:42 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u908947830_multivendor'@'localhost' (using password: YES) C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-12-16 10:44:42 --> Unable to connect to the database
+ERROR - 2021-12-16 10:44:45 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u908947830_multivendor'@'localhost' (using password: YES) C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-12-16 10:44:45 --> Unable to connect to the database
+ERROR - 2021-12-16 11:21:55 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u908947830_multivendor'@'localhost' (using password: YES) C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-12-16 11:21:55 --> Unable to connect to the database
+ERROR - 2021-12-16 11:21:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u908947830_multivendor'@'localhost' (using password: YES) C:\xamppupdatednew\htdocs\Ecommerce-CodeIgniter-Bootstrap-master\Ecommerce-CodeIgniter-Bootstrap-master\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-12-16 11:21:56 --> Unable to connect to the database
